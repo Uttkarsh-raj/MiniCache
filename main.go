43eb9b5abc11283a721167cbcb43bdd6b2bed0b4
Uttkarsh-raj <uttkarsh.raj.jsr@gmail.com>
2024-04-01 20:17:42 +0530

    comments

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ import (
 func main() {
 	NewDb := database.CreateDatabase()            // Initialize / Create a new Database
 	NewList := model.CreateNewCommandsList()      // Initialize a commands queue to store the incoming requests
-	handler.Initialize(NewDb, NewList)            // Read data if already present / Persistent storage
+	handler.Initialize(NewDb, NewList)            // Read data if already present / Persistent storage if want to use as a normal db
 	server := gin.New()                           // New server
 	server.Use(gin.Logger())                      // Use gin's logger
 	routes.IncomingRoutes(server, NewDb, NewList) // add the routes to the server
